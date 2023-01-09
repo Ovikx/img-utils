a492e8be44171367f40606df877094aebfb21569
Ovikx <ovikdas10@gmail.com>
2023-01-09 16:47:17 -0600

    add noise function

---
+++ src/blank/generators.rs
@@ -0,0 +1,20 @@
+use image;
+use rand;
+
+/// Returns an ImageBuffer containing non-interpolated noise
+/// 
+/// ## Arguments
+/// 
+/// * `imgx` - Width of image
+/// * `imgx` - Height of image
+/// 
+pub fn noise(imgx: u32, imgy: u32) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+
+    for pixel in imgbuf.pixels_mut() {
+        let val = (rand::random::<f32>()*256.0) as u8;
+        *pixel = image::Rgb([val, val, val]);
+    };
+    
+    imgbuf
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use img_utils;
+
 fn main() {
-    println!("Hello, world!");
+    let img_buffer = img_utils::blank::generators::noise(256, 256);
+    img_buffer.save("test.png").unwrap();
 }
