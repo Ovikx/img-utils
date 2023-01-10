a2a5a5632ee3f91392ea51b1b8cff36b7ac9a2bf
Ovikx <ovikdas10@gmail.com>
2023-01-09 23:01:04 -0600

    fix noise method

--- src/blank/generators.rs
+++ src/blank/generators.rs
@@ -1,5 +1,6 @@
 use image;
 use rand;
+use vectorize::Matrix;
 
 /// Returns an ImageBuffer containing non-interpolated noise
 /// 
@@ -9,8 +10,7 @@ use rand;
 /// * `imgx` - Height of image
 /// 
 pub fn noise(imgx: u32, imgy: u32) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
-
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);    
     for pixel in imgbuf.pixels_mut() {
         let val = (rand::random::<f32>()*256.0) as u8;
         *pixel = image::Rgb([val, val, val]);
